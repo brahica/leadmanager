3d7cdd4fe17eb5d4adcdc4aa0f5c7a27dd7c5c65
LowDev <aurelien.brahic@gmail.com>
2020-03-04 16:22:21 +0100

    Starting Layout and components

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+
+import Header from './layout/Header';
+import DashBoard from './leads/DashBoard'
 
 class App extends Component {
     render() {
-        return <h1>React App</h1>
+        return (
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <DashBoard />
+                </div>
+            </Fragment>
+        )
     }
 }
 
---
+++ leadmanager/frontend/src/components/leads/DashBoard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Leads from './Leads'
+import Form from './Form'
+
+export default function DashBoard() {
+    return (
+        <Fragment>
+            <Form />
+            <Leads />
+        </Fragment>
+    )
+}
