049c1d786adb70b8cfae65048a6d2dd767fa4374
LowDev <aurelien.brahic@gmail.com>
2020-03-05 17:00:09 +0100

    Allow * Host for local dev

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '49lh0^3%qe!3aa%eo1%_64izk1&t7p)0g(c8bb-b3c$4g9^#@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
