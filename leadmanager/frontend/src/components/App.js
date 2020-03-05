b6a8b9a4288fde1b5e8fbede0adbaaaa17838c8a
LowDev <aurelien.brahic@gmail.com>
2020-03-05 13:51:06 +0100

    Add store en components layout to main App

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,18 +1,23 @@
-import React, { Component, Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from 'react'
+import ReactDOM from 'react-dom'
 
-import Header from './layout/Header';
+import Header from './layout/Header'
 import DashBoard from './leads/DashBoard'
+
+import { Provider } from 'react-redux'
+import store from '../store'
 
 class App extends Component {
     render() {
         return (
-            <Fragment>
-                <Header />
-                <div className="container">
-                    <DashBoard />
-                </div>
-            </Fragment>
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
+                    <div className="container">
+                        <DashBoard />
+                    </div>
+                </Fragment>
+            </Provider>
         )
     }
 }
