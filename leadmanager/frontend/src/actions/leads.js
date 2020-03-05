e4c376fa0682a61ceee4209a6bda09e037591fab
LowDev <aurelien.brahic@gmail.com>
2020-03-05 14:49:39 +0100

    Add New Action ADD_LEAD

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { GET_LEADS, DELETE_LEAD } from './types'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types'
 
 // GET LEADS
 export const getLeads = () => dispatch => {
@@ -21,6 +21,18 @@ export const deleteLead = (id) => dispatch => {
             dispatch({
                 type: DELETE_LEAD,
                 playload: id
+            })
+        })
+        .catch(err => console.error(err))
+}
+
+// ADD LEAD
+export const addLead = (lead) => dispatch => {
+    axios.post('/api/leads/', lead)
+        .then(res => {
+            dispatch({
+                type: ADD_LEAD,
+                playload: res.data
             })
         })
         .catch(err => console.error(err))
