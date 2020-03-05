00ec86ecec85a5e185d0ed94fada2ff84f3e46ad
LowDev <aurelien.brahic@gmail.com>
2020-03-05 17:03:43 +0100

    Add Dispatch to CreateMessage and Handle Errors

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types'
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types'
+
+import { createMessage } from '../actions/message'
 
 // GET LEADS
 export const getLeads = () => dispatch => {
@@ -18,6 +20,9 @@ export const getLeads = () => dispatch => {
 export const deleteLead = (id) => dispatch => {
     axios.delete(`/api/leads/${id}/`)
         .then(res => {
+            dispatch(createMessage({
+                deleteLead: 'Lead Deleted'
+            }))
             dispatch({
                 type: DELETE_LEAD,
                 playload: id
@@ -30,10 +35,22 @@ export const deleteLead = (id) => dispatch => {
 export const addLead = (lead) => dispatch => {
     axios.post('/api/leads/', lead)
         .then(res => {
+            dispatch(createMessage({
+                addLead: 'Lead Added'
+            }))
             dispatch({
                 type: ADD_LEAD,
                 playload: res.data
             })
         })
-        .catch(err => console.error(err))
+        .catch(err => {
+            const errors = {
+                msg: err.response.data,
+                status: err.response.status
+            }
+            dispatch({
+                type: GET_ERRORS,
+                playload: errors
+            })
+        })
 }
