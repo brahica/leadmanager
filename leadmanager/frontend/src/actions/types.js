71b27cdf10977b0051baf1f597b57b890e27b654
LowDev <aurelien.brahic@gmail.com>
2020-03-06 18:37:01 +0100

    Add USER_LOADING , USER_LOADED, AUTH_ERROR action type

--- leadmanager/frontend/src/actions/types.js
+++ leadmanager/frontend/src/actions/types.js
@@ -9,3 +9,8 @@ export const GET_ERRORS = 'GET_ERRORS';
 
 // MESSAGE ACTIONS TYPE
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+
+// AUTH ACTION TYPE
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
